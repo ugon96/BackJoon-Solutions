1080ae9bfbd252b35de8ca14baec9237f6f0fd06
Yugon Hwang <ugon96@gmail.com>
2022-08-22 03:17:28 +0900

    second commit

--- src/Practice.java
+++ src/Practice.java
@@ -7,5 +7,7 @@ public class Practice {
     public static void main(String[] args) {
         double a = Math.ceil( 3.0/2 );
         System.out.println(a);
+        System.out.println("Update for git-hub");
+
     }
 }
